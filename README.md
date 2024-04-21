66eae5df1eeae8fd5fbe952c61869d42d212983e
Рустам Рудаков <9381804@gmail.com>
2024-04-21 18:28:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Chebyshev polynomials for calculating position and velocity of celestial objects
+using NASA JPL ephemerides
---
+++ cd_cheb_go.go
@@ -0,0 +1,37 @@
+package chebyshev
+
+//Chebyshev polynomials for calculating position and velocity of celestial objects
+// using NASA JPL ephemerides
+
+func Chebyshev(order int, x float64, data []float64) float64 {
+
+	// Evaluate a Chebyshev polynomial
+	var bk float64
+	two_x := 2 * x
+	bkp2 := data[order]
+	bkp1 := two_x*bkp2 + data[order-1]
+
+	for n := order - 2; n > 0; n-- {
+		bk = data[n] + two_x*bkp1 - bkp2
+		bkp2 = bkp1
+		bkp1 = bk
+	}
+	return data[0] + x*bkp1 - bkp2
+}
+
+func DerChebyshev(order int, x float64, data []float64) float64 {
+	//Evaluate the derivative of a Chebyshev polynomial
+
+	var bk float64
+	two_x := 2 * x
+	bkp2 := float64(order) * data[order]
+	bkp1 := two_x*bkp2 + float64(order-1)*data[order-1]
+
+	for n := order - 2; n > 1; n-- {
+		bk = float64(n)*data[n] + two_x*bkp1 - bkp2
+		bkp2 = bkp1
+		bkp1 = bk
+	}
+
+	return data[1] + two_x*bkp1 - bkp2
+}
